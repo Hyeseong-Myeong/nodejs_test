f66734b36063cae668c4aed760d0a85092fe4969
Hyeseong-int <a01025819869@gmail.com>
2022-03-08 17:34:16 +0900

    setup routing and static dir

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
+
+
 const app = express()
 
+//start server
 app.listen(3000, function(){
     console.log("app start on port 3000");
+})
+
+//static directory
+app.use(express.static('public'))
+
+//url routing
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/public/main.html");
 })
