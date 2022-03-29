48c628a4a3f07b75ac0774caedd176c653b2d91c
Hyeseong-int <a01025819869@gmail.com>
2022-03-29 17:31:35 +0900

    passport js setup, local strategy, router

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const router = require('./router/index');
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const flash = require('connect-flash');
 
 //start server
 app.listen(3000, function(){
@@ -13,6 +16,14 @@ app.listen(3000, function(){
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 app.set('view engine', 'ejs');
 
--- router/join/index.js
+++ router/join/index.js
@@ -2,39 +2,53 @@ const { randomInt } = require('crypto');
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const passport = require('passport');
 const router = express.Router();
 const path = require('path');
+const LocalStrategy = require('passport-local').Strategy;
 //config info separation
 const config = require('../../config/config');
 
-var connection = mysql.createConnection(config.databaseOptions);
-
-// //DB setting
-// let connection = mysql.createConnection({
-//     host:'localhost',
-//     port: 3306,
-//     user: 'root',
-//     //git commit 전 수정 필수
-//     database: 'nodejs_prtc'
-// });
+let connection = mysql.createConnection(config.databaseOptions);
+//DB setting
 // connection.connect();
 
 //router
 router.get('/',function(req, res){
-    res.sendFile(path.join(__dirname, '../../public/join.html'));
+    res.render('join.ejs');    
 })
 
-router.post('/', function(req, res){
-    let body = req.body;
-    let email = body.email;
-    let name = body.name;
-    let password = body.password;
-    let uid = body.uid;
+//passport-local strategy
+passport.use('local-join', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passsReqToCallback: true
+    }, function(req, email, password, done){
+        console.log('local-join callback called');
+    }
+));
 
-    let query = connection.query('insert into user (email,name,pw,uid) values ("' + email + '","' + name + '","' + password + '","' + uid + '")',function(err, rows){
-        if(err) {throw err;}
-        else console.log("DB insert Done!");
-    })
-})
+router.post('/', passport.authenticate('local-join', {
+    successRedirect: '/main',
+    failureRedirect: '/join',
+    failureFlash: true
+}))
+
+// router.post('/', function(req, res){
+//     let body = req.body;
+//     let email = body.email;
+//     let name = body.name;
+//     let password = body.password;
+//     let uid = body.uid;
+
+//     let sql = {email: email, name : name, pw: password, uid : uid};
+//     let query = connection.query('insert into user set ?', sql ,function(err, rows){
+//         if(err) {throw err;}
+//         else {
+//             console.log("DB insert Done!");
+//             res.render('welcome.ejs', {'name' : name, 'id': uid})
+//         }
+//     })
+// })
 
 module.exports = router;
